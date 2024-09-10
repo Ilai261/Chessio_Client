dd2afdfe1eaa82cd3cc3ca2652fa6a9ad6340f4a
IlaiAzaria <ilai.bagab@gmail.com>
2024-09-10 18:17:43 +0300

    added game controller comments

--- src/main/java/org/chessio/chessio_client/Controllers/ChessBoardController.java
+++ src/main/java/org/chessio/chessio_client/Controllers/ChessBoardController.java
@@ -1,8 +1,11 @@
 package org.chessio.chessio_client.Controllers;
 import javafx.fxml.FXML;
 
+
 public class ChessBoardController {
     // Handle the logic for the chess board here
+
+    //private GameManager gameManager;
 
     public ChessBoardController() {
         // Initialize chess board state and logic here
@@ -12,4 +15,20 @@ public class ChessBoardController {
     public void initialize() {
         // Setup the chess board (e.g., populate the grid with pieces)
     }
+
+//    public GameManager {
+//        private String enemyType;
+//
+//        public GameManager(String enemyType)
+//        {
+//            if(enemyType == "bot")
+//            {
+//                // set up bot stuff
+//            }
+//            else {
+//                //set up server stuff
+//            }
+//        }
+//    }
+
 }
