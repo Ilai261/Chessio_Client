ae083aa51dea54fca2ee7defa9e6b2857d6f79da
IlaiAzaria <ilai.bagab@gmail.com>
2024-09-16 18:41:43 +0300

    deleted comments

--- src/main/java/org/chessio/chessio_client/SceneControllers/RegisterController.java
+++ src/main/java/org/chessio/chessio_client/SceneControllers/RegisterController.java
@@ -103,27 +103,6 @@ public class RegisterController {
         passwordField.clear();
     }
 
-//    @FXML
-//    private void handleRegister() throws IOException {
-//        // Retrieve the text from the username and password fields
-//        String username = usernameField.getText();
-//        String password = passwordField.getText();
-//
-//        // Example logic for handling login (replace with your actual logic)
-//        System.out.println("Username: " + username);
-//        System.out.println("Password: " + password);
-//
-//        // Clear the text fields after login logic
-//        usernameField.clear();
-//        passwordField.clear();
-//
-//        if (username.equals("admin") && password.equals("admin")) {
-//            // Change the scene to home_screen.fxml instead of opening a new window
-//            //?We should simply use here an outside function that moves between scenes to have less code
-//            openNewScene("/org/chessio/chessio_client/home_screen.fxml");
-//        }
-//    }
-
     public void openNewScene(String fxmlFilePath) throws IOException {
         // Load the new FXML file
         clientHttpSender.shutdown();
